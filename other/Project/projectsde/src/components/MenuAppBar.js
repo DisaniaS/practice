7d471ee159f89b2478d010d291ac9f80d917a2b0
DisaniaS <serzh.30303@mail.ru>
2022-07-13 00:18:17 +0300

    project-on-react ver2.1

--- other/Project/projectsde/src/components/MenuAppBar.js
+++ other/Project/projectsde/src/components/MenuAppBar.js
@@ -12,14 +12,48 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import PropTypes from 'prop-types';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
 import './components.css'
 
-const MenuAppBar = () => {
+
+
+
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
 
   return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+function MenuAppBar(props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
       <AppBar className='AppBar' position="fixed">
-        <Container className='Container' fixed>
+        <Container className='Container' fixed >
           <Toolbar className='Toolbar'>
             <Button color="inherit">Главная</Button>
             <Button color="inherit">Цены</Button>
@@ -28,7 +62,10 @@ const MenuAppBar = () => {
           </Toolbar>
         </Container>
       </AppBar>
-
+      </HideOnScroll>
+      <Toolbar />
+    </React.Fragment>
   );
-};
+}
+
 export default MenuAppBar;
